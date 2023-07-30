7d701296967ce238a673dfd8725be33e00140b33
MattFor <matthew-forester@protonmail.com>
2023-07-31 00:36:38 +0200

    Improvements

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
+/**
+ * Grr stop looking here all is available at
+ * @link https://github.com/MattFor/MattFor.github.io
+ */
+
+const setInitialTheme = () => {
+    const theme = localStorage.getItem('theme');
+    const themeButton = document.getElementById('themeButton');
+
+    if (theme === 'dark') {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        themeButton.innerText = 'Light Mode';
+    } else if (theme === 'light') {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        themeButton.innerText = 'Dark Mode';
+    }
+};
+
 const toggleTheme = () => {
     const themeButton = document.getElementById('themeButton');
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
-        themeButton.innerText = 'Dark Mode'; // Change to Dark Mode when light mode is active
+        themeButton.innerText = 'Dark Mode';
+        localStorage.setItem('theme', 'light');
     } else {
         document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
-        themeButton.innerText = 'Light Mode'; // Change to Light Mode when dark mode is active
+        themeButton.innerText = 'Light Mode';
+        localStorage.setItem('theme', 'dark');
     }
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (document.body.classList.contains('dark-mode')) {
-        document.getElementById('themeButton').innerText = 'Light Mode';
-    } else {
-        document.getElementById('themeButton').innerText = 'Dark Mode';
-    }
+    setInitialTheme();
 });
