4c8c6b78c43205bbe5c01b3c92a07a64a687bacb
MattFor <matthew-forester@protonmail.com>
2023-07-30 21:13:28 +0200

    Upgrades people upgrades

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const toggleDarkMode = () => {
     const themeButton = document.getElementById('themeButton');
     if (document.body.classList.contains('dark-mode')) {
-        document.body.classList.remove('dark-mode')
+        document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
-        themeButton.innerText = 'Light Mode';
+        themeButton.innerText = 'Dark Mode'; // Change to Dark Mode when light mode is active
     } else {
         document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
-        themeButton.innerText = 'Dark Mode';
+        themeButton.innerText = 'Light Mode'; // Change to Light Mode when dark mode is active
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     if (document.body.classList.contains('dark-mode')) {
-        document.getElementById('themeButton').innerText = 'Dark Mode';
-    } else {
         document.getElementById('themeButton').innerText = 'Light Mode';
+    } else {
+        document.getElementById('themeButton').innerText = 'Dark Mode';
     }
 });
