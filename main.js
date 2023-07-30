afea9b6db55b43f669828304ccd899565d617b3c
MattFor <matthew-forester@protonmail.com>
2023-07-30 20:32:24 +0200

    Updating....

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const toggleDarkMode = () => {
-    if (document.body.classList.contains('dark-mode'))
-    {
+    const themeButton = document.getElementById('themeButton');
+    if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode')
-        document.body.classList.add('light-mode')
-    }
-    else
-    {
-        document.body.classList.remove('light-mode')
-        document.body.classList.add('dark-mode')
+        document.body.classList.add('light-mode');
+        themeButton.innerText = 'Light Mode';
+    } else {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        themeButton.innerText = 'Dark Mode';
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.body.classList.contains('dark-mode')) {
+        document.getElementById('themeButton').innerText = 'Dark Mode';
+    } else {
+        document.getElementById('themeButton').innerText = 'Light Mode';
+    }
+});
