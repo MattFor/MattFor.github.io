d39e43f20f6412017f6d4c07dbdde19ba5e6cff4
MattFor <matthew-forester@protonmail.com>
2023-07-31 21:37:18 +0200

    no music ;(

--- js/subpagebar.js
+++ js/subpagebar.js
@@ -1,21 +1,23 @@
 const subpages = [
-    { name: 'Terms of Service', link: '/subpages/terms-of-service.html' },
-    { name: 'Privacy Policy', link: '/subpages/privacy-policy.html' },
-    { name: 'Changelog', link: 'changelog.html' },
-    { name: 'Credits', link: '/subpages/credits.html' }
+    { name: 'Terms of Service', link: 'subpages/terms-of-service.html' },
+    { name: 'Privacy Policy', link: 'subpages/privacy-policy.html' },
+    { name: 'Changelog', link: 'subpages/changelog.html' },
+    { name: 'Credits', link: 'subpages/credits.html' }
 ];
 
 function generateSubpagesBar() {
     const currentPage = window.location.pathname;
     let htmlContent = '';
+    const isSubpage = currentPage.includes('/subpages/');
 
     if (currentPage !== '/' && currentPage !== '/index.html') {
         htmlContent += '<a href="/" class="btn-main-page" id="back-to-main">Back to Main Page</a> | ';
     }
 
     subpages.forEach((page, index) => {
-        if (currentPage !== page.link) {
-            htmlContent += `<a href="${page.link}">${page.name}</a>`;
+        const link = isSubpage ? page.link.replace('subpages/', '') : page.link;
+        if (currentPage !== link) {
+            htmlContent += `<a href="${link}">${page.name}</a>`;
             if (index < subpages.length - 1) {
                 htmlContent += ' | ';
             }
