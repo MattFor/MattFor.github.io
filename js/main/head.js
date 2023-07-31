cb23a74e25fcd6689e240e025224efa5807a0ebf
MattFor <matthew-forester@protonmail.com>
2023-07-31 22:22:50 +0200

    update

--- js/main/head.js
+++ js/main/head.js
@@ -1,4 +1,27 @@
 (() => {
+    const subpages = [
+        {
+            name: "Terms of Service",
+            link: "subpages/terms-of-service.html",
+            description: "Read the Terms of Service for using Relaxy!, the Multipurpose Discord Bot."
+        },
+        {
+            name: "Privacy Policy",
+            link: "subpages/privacy-policy.html",
+            description: "Read the Privacy Policy for using Relaxy!, the Multipurpose Discord Bot."
+        },
+        {
+            name: "Changelog",
+            link: "subpages/changelog.html",
+            description: "Check out the changelog and see the latest changes for Relaxy!, the Multipurpose Discord Bot."
+        },
+        {
+            name: "Credits",
+            link: "subpages/credits.html",
+            description: "Credits and acknowledgments for those who have contributed to the development of Relaxy!, the Multipurpose Discord Bot."
+        }
+    ];
+
     const currentPage = window.location.pathname;
 
     let content = "Relaxy is a multipurpose with advanced features such as censoring, custom forums, heartboards, and more. Created by mattfor/MattFor#9884. Over 100000 users on 80+ servers."
