f815cb924b13d2ed15b93d8810f5e77f6ced7bc0
MattFor <matthew-forester@protonmail.com>
2024-08-01 15:32:07 +0200

    w

--- js/main/subpagebar.js
+++ js/main/subpagebar.js
@@ -2,22 +2,22 @@
     const subpages = [
         {
             name: "Terms of Service",
-            link: "subpages/terms-of-service/",
+            link: "subpages/terms-of-service.html",
             description: "Read the Terms of Service for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Privacy Policy",
-            link: "subpages/privacy-policy/",
+            link: "subpages/privacy-policy.html",
             description: "Read the Privacy Policy for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Changelog",
-            link: "subpages/changelog/",
+            link: "subpages/changelog.html",
             description: "Check out the changelog and see the latest changes for Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Credits",
-            link: "subpages/credits/",
+            link: "subpages/credits.html",
             description: "Credits and acknowledgments for those who have contributed to the development of Relaxy!, the Multipurpose Discord Bot."
         }
     ];
