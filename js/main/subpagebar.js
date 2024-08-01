5acf593fcafcbd292b554ff711518ca04a165479
MattFor <matthew-forester@protonmail.com>
2024-08-01 15:38:54 +0200

    w

--- js/main/subpagebar.js
+++ js/main/subpagebar.js
@@ -2,22 +2,22 @@
     const subpages = [
         {
             name: "Terms of Service",
-            link: "subpages/terms-of-service/",
+            link: "subpages/terms-of-service",
             description: "Read the Terms of Service for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Privacy Policy",
-            link: "subpages/privacy-policy/",
+            link: "subpages/privacy-policy",
             description: "Read the Privacy Policy for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Changelog",
-            link: "subpages/changelog/",
+            link: "subpages/changelog",
             description: "Check out the changelog and see the latest changes for Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Credits",
-            link: "subpages/credits/",
+            link: "subpages/credits",
             description: "Credits and acknowledgments for those who have contributed to the development of Relaxy!, the Multipurpose Discord Bot."
         }
     ];
@@ -26,21 +26,23 @@
     const isSubpage = currentPage.includes('/subpages/');
 
     let htmlContent = '';
-
-    if (currentPage !== '/' && currentPage !== '/index.html') {
+    if (currentPage !== '/' && currentPage !== '/index.html') 
+    {
         htmlContent += '<a href="/" class="btn-main-page" id="back-to-main">Back to Main Page</a> | ';
     }
 
     subpages.filter(p => !currentPage.includes(p.link)).forEach((page, index) => {
-        const link = isSubpage ? page.link.replace('subpages/', '') : page.link;
+        const link = isSubpage ? page.link.replace(/subpages\/.*/, '') : page.link;
 
         htmlContent += `<a href="${link}">${page.name}</a>`;
-        if (index < subpages.length - 1) {
+        if (index < subpages.length - 1) 
+        {
             htmlContent += ' | ';
         }
     });
 
-    if (htmlContent.endsWith(' | ')) {
+    if (htmlContent.endsWith(' | ')) 
+    {
         htmlContent = htmlContent.slice(0, -3);
     }
 
