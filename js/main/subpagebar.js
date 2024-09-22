a94822dd350a43bbce1879df8663634f08034695
MattFor <matthew-forester@protonmail.com>
2024-09-22 05:05:39 +0200

    Making this functional again.

--- js/main/subpagebar.js
+++ js/main/subpagebar.js
@@ -2,49 +2,47 @@
     const subpages = [
         {
             name: "Terms of Service",
-            link: "subpages/terms-of-service.html",
+            link: "/subpages/terms-of-service.html",
             description: "Read the Terms of Service for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Privacy Policy",
-            link: "subpages/privacy-policy.html",
+            link: "/subpages/privacy-policy.html",
             description: "Read the Privacy Policy for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Changelog",
-            link: "subpages/changelog.html",
+            link: "/subpages/changelog.html",
             description: "Check out the changelog and see the latest changes for Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Credits",
-            link: "subpages/credits.html",
+            link: "/subpages/credits.html",
             description: "Credits and acknowledgments for those who have contributed to the development of Relaxy!, the Multipurpose Discord Bot."
         }
     ];
 
     const currentPage = window.location.pathname;
-    const isSubpage = currentPage.includes('subpages');
 
     let htmlContent = "";
-    if (currentPage !== "/" && currentPage !== "/terms-of-service.html")
+    if (currentPage !== "/" && currentPage !== "/subpages/terms-of-service.html")
     {
         htmlContent += '<a href="/" class="btn-main-page" id="back-to-main">Back to Main Page</a> | ';
     }
 
     subpages.filter(p => !currentPage.includes(p.link)).forEach((page, index) => {
-        const link = isSubpage ? page.link.replace(/subpages\/.*\//, '') : page.link;
+        htmlContent += `<a href="${page.link}">${page.name}</a>`;
 
-        htmlContent += `<a href="${link}">${page.name}</a>`;
-        if (index < subpages.length - 1) 
+        if (index < subpages.length - 1)
         {
-            htmlContent += ' | ';
+            htmlContent += " | ";
         }
     });
 
-    if (htmlContent.endsWith(' | ')) 
+    if (htmlContent.endsWith(" | "))
     {
-        htmlContent = htmlContent.slice(0, -3);
+        htmlContent = htmlContent.slice(0, -3); // Remove trailing separator
     }
 
-    document.getElementById('subpages-bar').innerHTML = htmlContent;
+    document.getElementById("subpages-bar").innerHTML = htmlContent;
 })();
