67c8f8a5688e1bd80b23237334fdecc5ab2eff9e
MattFor <matthew-forester@protonmail.com>
2024-09-22 04:54:37 +0200

    Making this functional again.

--- js/main/subpagebar.js
+++ js/main/subpagebar.js
@@ -2,22 +2,22 @@
     const subpages = [
         {
             name: "Terms of Service",
-            link: "subpages/terms-of-service/terms-of-service.html",
+            link: "subpages/terms-of-service.html",
             description: "Read the Terms of Service for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Privacy Policy",
-            link: "subpages/privacy-policy/terms-of-service.html",
+            link: "subpages/privacy-policy.html",
             description: "Read the Privacy Policy for using Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Changelog",
-            link: "subpages/changelog/terms-of-service.html",
+            link: "subpages/changelog.html",
             description: "Check out the changelog and see the latest changes for Relaxy!, the Multipurpose Discord Bot."
         },
         {
             name: "Credits",
-            link: "subpages/credits/terms-of-service.html",
+            link: "subpages/credits.html",
             description: "Credits and acknowledgments for those who have contributed to the development of Relaxy!, the Multipurpose Discord Bot."
         }
     ];
